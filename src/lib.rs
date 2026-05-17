29187dbfd8b775dd768813bb97104b54c8f3b0ff
Aleksandar Hubanov <al.hubanov@gmail.com>
2026-05-17 10:23:13 +0300

    refactor: rename + prep scandi

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use rand::{self, seq::SliceRandom};
 use wasm_bindgen::prelude::*;
 
-use crate::grid::Grid;
+use crate::grid_simple::Grid;
 
-pub mod grid;
+pub mod grid_simple;
 pub mod input_utilities;
 
 static WORDS: &str = include_str!("../word_files/common_english_words_long.txt");
@@ -14,7 +14,7 @@ pub fn run() -> () {
     let width = input_utilities::take_dimension_value_as_input("width", &mut input);
     let height = input_utilities::take_dimension_value_as_input("height", &mut input);
 
-    let grid = match build_crossword_grid(width, height) {
+    let grid = match build_crossword_grid_for_command_line(width, height) {
         Ok(res) => res,
         Err(s) => { 
             println!("{}", s); 
@@ -22,12 +22,29 @@ pub fn run() -> () {
         } 
     };
 
-    // println!();
-    // println!("Here's a crossword: ");
-    // println!();
+    println!();
+    println!("Here's a crossword: ");
+    println!();
     
-    // grid.print();
+    grid.print();
 }
+
+pub fn build_crossword_grid_for_command_line(width: u32, height: u32) -> Result<Grid, String> {
+
+    let mut grid = Grid::new(width, height);
+    let mut words_vec : Vec<&str> = WORDS.lines().collect();
+
+    let mut rng = rand::rng();
+    words_vec.shuffle(&mut rng);
+
+    for word in words_vec {
+        if let Err(_) = grid.place_word(&word.to_owned()) {
+            continue;
+        }
+    }
+
+    return Ok(grid);
+} 
 
 #[wasm_bindgen]
 pub fn build_crossword_grid(width: u32, height: u32) -> Result<JsValue, String> {
