55fb07ed5cd1cb8160839933dd6d41062c9a1542
Aleksandar Hubanov <al.hubanov@gmail.com>
2026-06-04 12:24:56 +0300

    fix: redefine meaning of clue

--- src/grid_scandi/clue.rs
+++ src/grid_scandi/clue.rs
@@ -1,5 +1,4 @@
 use serde::{Serialize, Deserialize};
-use std::{collections::BTreeSet};
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Serialize, Deserialize, Copy, Clone)]
 pub enum SlotDirection {
@@ -10,12 +9,6 @@ pub enum SlotDirection {
 }
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Serialize, Deserialize)]
-struct AssociatedAnswer {
-    answer: String,
-    direction: SlotDirection,
-}
-
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Serialize, Deserialize)]
 pub struct Clue {
-    associated_answers: BTreeSet<AssociatedAnswer>
+    hint: String
 }
